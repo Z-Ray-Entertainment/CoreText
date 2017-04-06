5798466737d8f14c4475846ed4b6f2f615453f58
Vortex Aherontic <vortex@z-ray.de>
2017-04-06 09:28:14 +0200

    changes Pakage order and main pakage name

---
+++ src/de/zray/coretex/VariableStore.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.zray.coretex;
+
+import de.zray.coretex.exceptions.UnknownVariableException;
+import de.zray.coretex.exceptions.InvalidVarTypeException;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author Vortex Acherontic
+ */
+public class VariableStore {
+    private List<Variable> vars = new LinkedList<>();
+    
+    public void setVariable(String name, String value) throws InvalidVarTypeException{
+        for(Variable tmp : vars){
+            if(tmp.compare(name)){
+                tmp.setValue(value);
+                return;
+            }
+        }
+    }
+    
+    public void addVariable(Variable var){
+        for(Variable tmp : vars){
+            if(tmp.compare(var.getName())){
+                tmp = var;
+                return;
+            }
+        }
+        vars.add(var);
+    }
+    
+    public Variable getVariable(String name) throws UnknownVariableException{
+        for(Variable tmp : vars){
+            if(tmp.compare(name)){
+                return tmp;
+            }
+        }
+        throw new UnknownVariableException(name);
+    }
+}
---
+++ src/de/zray/coretex/exceptions/InvalidParameterValueException.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.zray.coretex.exceptions;
+
+import de.zray.coretex.Parameter;
+
+/**
+ *
+ * @author Vortex Acherontic
+ */
+public class InvalidParameterValueException extends Exception{
+
+    public InvalidParameterValueException(Parameter.Type inputType, Parameter.Type setType) {
+        super(inputType.toString()+" is invalid you must set a value as "+setType);
+    }
+    
+    public InvalidParameterValueException(String msg) {
+        super(msg);
+    }
+}
