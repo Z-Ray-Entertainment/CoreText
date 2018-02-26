ca5f92c0ccb0b39970de59a3714f61524f1c8085
Vortex Acherontic <vortex@z-ray.de>
2018-02-26 09:46:05 +0100

    Fixed parameters not build in Parser and added debug output

--- src/de/zray/coretex/script/Parser.java
+++ src/de/zray/coretex/script/Parser.java
@@ -16,7 +16,7 @@ public class Parser {
     public List<ScriptElement> parseScript(String script){
         List<ScriptElement> elements = new LinkedList<>();
         int start = 0;
-        boolean closeState = false, state = false, command = false;
+        boolean closeState = false, state = false, command = true;
         
         for(int i = 0; i < script.length(); i++){
             String curChar = script.substring(i, i+1);
@@ -24,6 +24,7 @@ public class Parser {
                 case "<" :
                     if(start == -1){
                         state = true;
+                        command = false;
                         start = i;
                     }
                     break;
@@ -32,10 +33,14 @@ public class Parser {
                         String content = script.substring(start, i+1);
                         if(!closeState){
                             elements.add(new ScriptElement(ScriptElement.Type.OPEN_STATE, content));
+                            System.out.println("[Parser]: Found opening state: "+content);
+                            command = true;
                         }
                         else{
                             elements.add(new ScriptElement(ScriptElement.Type.CLOSE_STATE, content));
                             closeState = false;
+                            command = true;
+                            System.out.println("[Parser]: Found closing state: "+content);
                         }
                         start = -1;
                     }
@@ -47,40 +52,58 @@ public class Parser {
                     break;
                 case "(" :
                     elements.add(new ScriptElement(ScriptElement.Type.CLIP_OPEN, curChar));
+                    start = i+1;
                     break;
                 case ")" :
                     elements.add(new ScriptElement(ScriptElement.Type.CLIP_CLOSE, curChar));
+                    start = i+1;
                     break;
                 case "\"" :
                     elements.add(new ScriptElement(ScriptElement.Type.STRING_CHARACTER, curChar));
+                    start = i+1;
                     break;
                 case "[" :
                     elements.add(new ScriptElement(ScriptElement.Type.CODE_START, curChar));
+                    start = i+1;
                     break;
                 case "]" :
                     elements.add(new ScriptElement(ScriptElement.Type.CODE_END, curChar));
+                    start = i+1;
                     break;
                 case ";" :
+                    if(!command && !closeState && !state){
+                        elements.add(buildParameter(script, start, i));
+                    } else if(command && !closeState && !state){
+                        elements.add(buildCommand(script, start, i));
+                    }
                     elements.add(new ScriptElement(ScriptElement.Type.COMMAD_END, curChar));
-                    command = false;
+                    start = i+1;
                     break;
                 case " " :
-                    String content = script.substring(start, i);
-                    if(!command){
-                        elements.add(new ScriptElement(ScriptElement.Type.COMMAND, content));
-                        command = true;
+                    if(command){
+                        elements.add(buildCommand(script, start, i));
+                        command = false;
+                        start = i+1;
                     }
                     else{
-                        elements.add(new ScriptElement(ScriptElement.Type.PARAMETER, content));
-                    }
-                    break;
-                default : 
-                    if(start != -1){
-                        start = i;
+                        elements.add(buildParameter(script, start, i));
+                        start = i+1;
                     }
                     break;
             }
         }
         return elements;
+    }
+    
+    private ScriptElement buildCommand(String script, int start, int end){
+        String content = script.substring(start, end);
+        System.out.println("[Parser]: Found command: "+content);
+        return new ScriptElement(ScriptElement.Type.COMMAND, content);
+    }
+    
+    private ScriptElement buildParameter(String script, int start, int end){
+        String content = script.substring(start, end);
+        System.out.println("[Parser]: Found parameter: "+content);
+        return new ScriptElement(ScriptElement.Type.PARAMETER, content);
     }
 }
