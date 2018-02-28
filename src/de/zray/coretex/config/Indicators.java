86e9b5a064b447be0d85a453dae82498fec3b911
Vortex Acherontic <vortex@z-ray.de>
2018-02-28 09:42:10 +0100

    Added Indicators for later settings purpose

---
+++ src/de/zray/coretex/config/Indicators.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.zray.coretex.config;
+
+/**
+ *
+ * @author vortex
+ */
+public class Indicators {
+    public static String STRING = "\"", NESTED_COMMAND_START = "(", NESTED_COMMAND_END = ")",
+            CODE_START = "[", CODE_END = "]", COMMAND_END = ";", SEPERATOR = " ",
+            STATE_OPEN = "<", STATE_CLOSE = ">", CLOSING_STATE = "/";
+}
